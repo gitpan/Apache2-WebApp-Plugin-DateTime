a86d8571edddbbabee3bfefc2a953253a7a67a28
Marc S. Brooks <mbrooks@cpan.org>
2011-01-08 13:19:16 +0000

    Import of MBROOKS/Apache2-WebApp-Plugin-DateTime-0.06 from CPAN.
    gitpan-cpan-distribution: Apache2-WebApp-Plugin-DateTime
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         MBROOKS/Apache2-WebApp-Plugin-DateTime-0.06.tar.gz
    gitpan-cpan-author:       MBROOKS
    gitpan-cpan-maturity:     released

--- lib/Apache2/WebApp/Plugin/DateTime.pm
+++ lib/Apache2/WebApp/Plugin/DateTime.pm
@@ -23,7 +23,7 @@ use Params::Validate qw( :all );
 use POSIX qw( strftime );
 use Time::ParseDate;
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~[  OBJECT METHODS  ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
@@ -168,6 +168,6 @@ Marc S. Brooks, E<lt>mbrooks@cpan.orgE<gt> - L<http://mbrooks.info>
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-See L<http://www.perl.com/perl/misc/Artistic.html>
+See L<http://dev.perl.org/licenses/artistic.html>
 
 =cut
